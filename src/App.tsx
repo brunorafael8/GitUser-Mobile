25dc2cfa5bf1ddde40265a2d295419cdffba61a5
brunorafael8 <bruninho_lito@hotmail.com>
2019-11-17 17:36:00 -0300

    (add) relay on src

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@
  */
 
 import React from 'react';
+import Config from "react-native-config";
+import Icon from 'react-native-vector-icons/Octicons';
 import styled from 'styled-components/native';
 
 const SafeArea = styled.SafeAreaView`
@@ -13,25 +15,74 @@ const Container = styled.View`
   background-color: #e90052;
   padding: 10px;
   height: 100%;
+  align-items: center;
+  justify-content: flex-start;
 `;
 
 const Title = styled.Text`
   color: #38003c;
   font-weight: bold;
   padding: 5px;
-  font-size: 30px;
+  font-size: 40px;
   border-bottom-color: #38003c;
   border-bottom-width: 3px;
+  margin-top: 40px;
+  letter-spacing: 6px;
 `;
 
-const App = () => (
+const Content = styled.View`
+  margin-top: 100px;
+  width: 80%;
+  height: 40%;
+  justify-content: center;
+`;
+
+const SearchInput = styled.TextInput`
+  height: 50px;
+  width: 100%;
+  border-radius: 5px;
+  border: 1px;
+  padding: 15px;
+  border-color: #38003c;
+  background-color: white;
+`;
+
+const SearchButton = styled.TouchableOpacity`
+  background-color: #38003c;
+  border-radius: 5px;
+  width: 100%;
+  margin-top: 15px;
+  padding: 15px;
+`
+const SearchButtonContent = styled.View``
+
+const SearchButtonText = styled.Text`
+  color: #fff;
+  font-size: 18;
+  font-weight: bold;
+  align-self: center;
+`
+
+const App = () =>{ 
+  console.log(Config)
+  return(
   <>
     <SafeArea>
       <Container>
         <Title>GITUSER</Title>
+        <Icon name="mark-github" size={50} color="#38003c" />
+
+        <Content>
+          <SearchInput placeholder="Ex: brunorafael8"></SearchInput>
+          <SearchButton>
+          <SearchButtonContent>
+            <SearchButtonText>Search</SearchButtonText>
+          </SearchButtonContent>
+        </SearchButton>
+        </Content>
       </Container>
     </SafeArea>
   </>
 );
-
+}
 export default App;
---
+++ src/relay/fetchQuery.tsx
@@ -0,0 +1,63 @@
+import Config from "react-native-config";
+import { UploadableMap, Variables } from 'react-relay';
+import { RequestNode } from 'relay-runtime';
+import fetchWithRetries from './fetchWithRetries';
+import { getHeaders, getRequestBody, handleData, isMutation } from './helpers';
+
+export const GRAPHQL_URL = 'https://api.github.com/graphql';
+
+// Define a function that fetches the results of a request (query/mutation/etc)
+// and returns its results as a Promise:
+const fetchQuery = async (
+  request: RequestNode,
+  variables: Variables,
+  uploadables: UploadableMap,
+) => {
+  try {
+    const body = getRequestBody(request, variables, uploadables);
+    const headers = {
+      ...getHeaders(uploadables),
+      Authorization: 'Bearer ' + Config.GITHUP_TOKEN,
+    };
+
+    const response = await fetchWithRetries(GRAPHQL_URL, {
+      method: 'POST',
+      headers,
+      body,
+      fetchTimeout: 20000,
+      retryDelays: [1000, 3000, 5000],
+    });
+
+    const data = await handleData(response);
+
+    if (response.status === 401) {
+      throw data.errors;
+    }
+
+    if (isMutation(request) && data.errors) {
+      throw data;
+    }
+
+    if (!data.data) {
+      throw data.errors;
+    }
+
+    return data;
+  } catch (err) {
+    // eslint-disable-next-line
+    console.log('err: ', err);
+
+    const timeoutRegexp = new RegExp(/Still no successful response after/);
+    const serverUnavailableRegexp = new RegExp(/Failed to fetch/);
+    if (
+      timeoutRegexp.test(err.message) ||
+      serverUnavailableRegexp.test(err.message)
+    ) {
+      throw new Error('Unavailable service. Try again later.');
+    }
+
+    throw err;
+  }
+};
+
+export default fetchQuery;
