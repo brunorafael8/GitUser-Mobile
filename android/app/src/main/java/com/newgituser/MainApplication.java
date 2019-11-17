9884edacda7e26c31e23dd9529c21e042b3c7a7b
brunorafael8 <bruninho_lito@hotmail.com>
2019-11-17 17:26:38 -0300

    (remove) react-native-vector-icons link

--- android/app/src/main/java/com/newgituser/MainApplication.java
+++ android/app/src/main/java/com/newgituser/MainApplication.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
